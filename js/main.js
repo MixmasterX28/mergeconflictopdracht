6985d0e5e5b5092cfc44a268792b6d5645eeb8e8
Pepijn te Winkel <37923@ma-web.nl>
2023-10-16 10:06:13 +0200

    extrahaiku

--- js/main.js
+++ js/main.js
@@ -8,10 +8,15 @@ function basicHaiku()
 {
     return ["Toward those short trees","We saw a batman descending","The sky was gray."]
 }
+function basicHaiku2()
+{
+    return ["All the leaves were brown","The sky was gray","California dreaming."]
+}
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
 haikus = [
     basicHaiku()
+    basicHaiku2()
 ]
 
 function randomHaiku()
